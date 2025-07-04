c56922f8521eeeaab4f3117b9c8d5045158c1bbd
welsondeivid <53789831+welsondeivid@users.noreply.github.com>
2025-07-04 17:24:01 -0300

    :wrench: fix: caminho dos models

--- routes/matchs.js
+++ routes/matchs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Match = require('../models/Match');
+const Match = require('../models/match');
 
 router.get('/', async (req, res) => {
   try {
